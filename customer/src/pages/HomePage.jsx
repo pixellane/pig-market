3734f92687f694c48768db3499fbc6c8ad2d7dad
Your Name <your@email.com>
2026-08-13 23:24:37 +0800

    fix missing useMemo import

--- customer/src/pages/HomePage.jsx
+++ customer/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useCart } from '../contexts/CartContext.jsx';
